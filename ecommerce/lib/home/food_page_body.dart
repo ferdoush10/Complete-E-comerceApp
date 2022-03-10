d0cd80beee031418e655571eef1963040500f30b
Ferdoush Wahid <ferdoushwahid74@gmail.com>
2022-03-11 01:19:07 +0600

    First Commit

---
+++ ecommerce/lib/home/food_page_body.dart
@@ -0,0 +1,57 @@
+import 'package:ecommerce/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  _FoodPageBodyState createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      color: AppColors.mainColor,
+      height: 320,
+      child: PageView.builder(
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 15, right: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF111111),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                "assets/image/food1.png",
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 150,
+            margin: const EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: AppColors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
